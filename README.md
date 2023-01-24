8113b27acc497eeb39853c8510b1e64453f16e2e
TheNamlessGuy <bragux@hotmail.com>
2023-01-24 21:40:41 +0100

    Version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # no-youtube-shorts
+
+A Firefox plugin for redirecting away from YouTube shorts
+
+https://addons.mozilla.org/firefox/addon/namless-no-youtube-shorts/
---
+++ background.js
@@ -0,0 +1,17 @@
+function onTabUpdate(tabID, changeInfo, tabInfo) {
+  if (changeInfo.url == null) { return; }
+  console.log('onTabUpdate', tabID, changeInfo, tabInfo);
+
+  const url = new URL(changeInfo.url);
+  let id = url.pathname;
+  if (!id.startsWith('/shorts/')) { return; }
+  id = id.substring(8); // 8 === '/shorts/'.length
+
+  browser.tabs.update(tabID, {url: `${url.protocol}//${url.hostname}/watch?v=${id}`});
+}
+
+async function main() {
+  browser.tabs.onUpdated.addListener(onTabUpdate);
+}
+
+main();
